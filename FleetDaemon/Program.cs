2a0c9678a17a302ba1bc66d3a7c7802a4586ed96
Alistair Woodcock <alistairwoodcock@gmail.com>
2016-08-20 20:20:47 +1000

    beginning file storage

--- FleetDaemon/Program.cs
+++ FleetDaemon/Program.cs
@@ -8,6 +8,7 @@ using System.ServiceModel;
 using System.Diagnostics;
 using System.IO;
 using FleetServer;
+using System.Net.NetworkInformation;
 
 namespace FleetDaemon
 {
@@ -24,12 +25,14 @@ namespace FleetDaemon
     class Daemon
     {
         private ServiceHost service;
+        private SimpleStorage Storage;
         private IFleetService FleetServer; // This will need to be populated with the
-                                            // actual client or whatever
-
+                                           // actual client or whatever
         public Daemon()
         {
             DaemonService.OnRequest += DaemonService_OnRequest;
+            this.FleetServer = new FleetServerStub();
+            this.Storage = new SimpleStorage("./filestore.json");
         }
 
         private void DaemonService_OnRequest(IPCMessage message)
@@ -57,10 +60,80 @@ namespace FleetDaemon
             Console.WriteLine("Daemon running. Press the any key to exit.");
             Console.WriteLine(Directory.GetCurrentDirectory());
 
-            Process.Start(@"..\..\..\FileShare\bin\Debug\FileShare.exe");
+            var clientReg = new FleetClientRegistration();
+            clientReg.FriendlyName = System.Environment.MachineName;
+            clientReg.IpAddress = "boop boop we gotta implement";
+            clientReg.MacAddress = (from nic in NetworkInterface.GetAllNetworkInterfaces()
+                                    where nic.OperationalStatus == OperationalStatus.Up
+                                    select nic.GetPhysicalAddress().ToString()
+                                    ).FirstOrDefault(); ;
 
-            
+            var clientToken = this.FleetServer.RegisterClient(clientReg);
+            this.Storage.store("token", clientToken);
+            Console.WriteLine("Client registered to Server.");
+
+            Process.Start(@"..\..\..\FileShare\bin\Debug\FileShare.exe");
             
         }
+        
+    }
+
+    public class FleetServerStub : IFleetService
+    {
+        public FleetFile GetFile(FleetClientToken token, FleetFileIdentifier fileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FleetMessage GetMessage(FleetClientToken token, FleetMessageIdentifier fileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FleetHearbeatEnum Heartbeat(FleetClientToken token, FleetClientIdentifier[] knownClients)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FleetFileIdentifier[] QueryFiles(FleetClientToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FleetMessageIdentifier[] QueryMessages(FleetClientToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FleetClientToken RegisterClient(FleetClientRegistration registrationModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendFileMultipleRecipient(FleetClientToken token, FleetClientIdentifier[] recipients, FleetFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendFileSingleRecipient(FleetClientToken token, FleetClientIdentifier recipient, FleetFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendMessageMultipleRecipient(FleetClientToken token, FleetClientIdentifier[] recipients, FleetMessage msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendMessageSingleRecipient(FleetClientToken token, FleetClientIdentifier recipient, FleetMessage msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class SimpleStorage
+    {
+        public SimpleStorage()
     }
 }
