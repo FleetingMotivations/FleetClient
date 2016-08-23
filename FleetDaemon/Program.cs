e720e25a27c9cd3668feb19cce57d7fc602b209d
Alistair Woodcock <alistairwoodcock@gmail.com>
2016-08-23 12:01:51 +1000

    refactored router into its own class

--- FleetDaemon/Program.cs
+++ FleetDaemon/Program.cs
@@ -39,8 +39,8 @@ namespace FleetDaemon
 
         private ServiceHost service;
         private SimpleStorage Storage;
+        private Router Router;
 
-        private IPCMessage FileShareMessage;
         private FleetClientToken ClientToken;
         private IFleetService FleetServer
         {
@@ -69,7 +69,8 @@ namespace FleetDaemon
         private Daemon()
         {
             DaemonService.OnRequest += DaemonService_OnRequest;
-            //this.FleetServer = null;
+            this.FleetServer = null;
+            this.Router = Router.Instance;
             this.Storage = new SimpleStorage("./filestore.json");
 
             var processes = new Dictionary<String, String>();
@@ -84,6 +85,104 @@ namespace FleetDaemon
         {
             Console.WriteLine(String.Format("Received message from: {0}, to: {1}", message.ApplicaitonSenderID, message.ApplicationRecipientID));
             Console.WriteLine(String.Format("Message Type: {0}", message.Content["type"]));
+
+            this.Router.HandleMessage(message);
+        }
+
+        public void HandleFileReceive(String filename)
+        {
+            Console.WriteLine(String.Format("Received new file from: {0}, to {1}, filename: {2}"), );
+            var message = new IPCMessage {
+                    
+            };
+            
+            this.Router.HandleMessage(message);
+            //TODO(AL): Calll Router to handle this and pass it to the appropriate client process
+        }
+
+        public void Run()
+        {
+            // Service initialisation
+            var address = new Uri("net.pipe://localhost/fleetdaemon");
+            var binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            this.service = new ServiceHost(typeof(DaemonService));
+            this.service.AddServiceEndpoint(typeof(IDaemonIPC), binding, address);
+            this.service.Open();
+
+            Console.WriteLine("Daemon IPC service listening");
+
+            // Create registration token
+            var clientReg = new FleetClientRegistration();
+            clientReg.FriendlyName = System.Environment.MachineName;
+            clientReg.IpAddress = "boop boop we gotta implement";
+            clientReg.MacAddress = (from nic in NetworkInterface.GetAllNetworkInterfaces()
+                                    where nic.OperationalStatus == OperationalStatus.Up
+                                    select nic.GetPhysicalAddress().ToString()
+                                    ).FirstOrDefault();
+
+            // Register with server
+            var client = FleetServer;
+            ClientToken = client.RegisterClient(clientReg);
+
+            Console.WriteLine("Received registration token");
+
+            // Start Router
+            Router.Instance.SetClientToken(ClientToken);
+
+            // Start heartbeat
+            HearbeatManager.WaitLength = 2000;
+            HearbeatManager.Instance.StartHeartbeat(ClientToken);
+
+            Console.WriteLine("Heartbeat is running");
+
+            // Other loading
+            // ????
+
+            // Daemon is running
+            Console.WriteLine("Daemon running. Press the any key to exit.");
+            Console.ReadLine();
+
+            //Console.WriteLine(Directory.GetCurrentDirectory());
+            //var clientToken = this.FleetServer.RegisterClient(clientReg);
+            //this.Storage.store("token", clientToken);
+            //this.Storage.store("token", "test_token_cool");
+            //Console.WriteLine("Client registered to Server.");
+ 
+            //Process.Start(@"..\..\..\FileShare\bin\Debug\FileShare.exe");
+        }
+    }
+
+    class Router
+    {
+        // Static instance handling
+        private static Router instance;
+        public static Router Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Router();
+                }
+                return instance;
+            }
+        }
+
+        private IPCMessage FileShareMessage;
+        private SimpleStorage Storage;
+        private FleetClientToken ClientToken;
+        public Router()
+        {
+            this.Storage = new SimpleStorage("./filestore.json");
+        }
+
+        public void SetClientToken(FleetClientToken tok)
+        {
+            this.ClientToken = tok;
+        }
+
+        public void HandleMessage(IPCMessage message)
+        {
 
             switch (message.LocationHandle)
             {
@@ -119,13 +218,13 @@ namespace FleetDaemon
 
                 case IPCMessage.MessageLocationHandle.DAEMON:
                     //NOTE(AL+JORDAN): These are all the messages directed for the daemon to handle
-                    
+
                     //TODO(AL+JORDAN): Worstations selected
                     //                 Accept or rejection of file
                     //                 
                     // workstationShareList, fileAccepted, 
 
-                    if(message.Type.Equals("workstationShareList"))
+                    if (message.Type.Equals("workstationShareList"))
                     {
                         //NOTE(AL): Workstation process is sending us the list of 
                         //          workstations we want to send a file to
@@ -153,7 +252,8 @@ namespace FleetDaemon
                         string fileName = filePath.Split(Path.DirectorySeparatorChar)
                             .Last();
 
-                        var file = new FleetFile{
+                        var file = new FleetFile
+                        {
                             FileContents = fileContent,
                             FileName = fileName
                         };
@@ -167,75 +267,21 @@ namespace FleetDaemon
                         var server = new FleetServiceClient();
                         server.SendFileMultipleRecipient(this.ClientToken, selectedClients.ToArray(), file);
                     }
-                    else if(message.Type.Equals("fileAccepted"))
+                    else if (message.Type.Equals("fileAccepted"))
                     {
                         // message.Content["accepted"] = true | false;
                         // message.Content["filePath"]
                         //TODO(AL+JORDAN):  open the file in the default thing
                     }
 
-                break;
+                    break;
                 default:
                     Console.WriteLine("SADFACE please figure out what to do here");
-                break;
+                    break;
 
-                Console.WriteLine("We got a file.");
-                Console.WriteLine(String.Format("File URL: {0}", message.Content["fileurl"]));
+                    Console.WriteLine("We got a file.");
+                    Console.WriteLine(String.Format("File URL: {0}", message.Content["fileurl"]));
             }
-        }
-
-        public void Run()
-        {
-            // Service initialisation
-            var address = new Uri("net.pipe://localhost/fleetdaemon");
-            var binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
-            this.service = new ServiceHost(typeof(DaemonService));
-            this.service.AddServiceEndpoint(typeof(IDaemonIPC), binding, address);
-            this.service.Open();
-
-            Console.WriteLine("Daemon IPC service listening");
-
-            // Create registration token
-            var clientReg = new FleetClientRegistration();
-            clientReg.FriendlyName = System.Environment.MachineName;
-            clientReg.IpAddress = "boop boop we gotta implement";
-            clientReg.MacAddress = (from nic in NetworkInterface.GetAllNetworkInterfaces()
-                                    where nic.OperationalStatus == OperationalStatus.Up
-                                    select nic.GetPhysicalAddress().ToString()
-                                    ).FirstOrDefault();
-
-            // Register with server
-            var client = FleetServer;
-            ClientToken = client.RegisterClient(clientReg);
-
-            Console.WriteLine("Received registration token");
-
-            // Start heartbeat
-            HearbeatManager.WaitLength = 2000;
-            HearbeatManager.Instance.StartHeartbeat(ClientToken);
-
-            Console.WriteLine("Heartbeat is running");
-
-            // Other loading
-            // ????
-
-            // Daemon is running
-            Console.WriteLine("Daemon running. Press the any key to exit.");
-            Console.ReadLine();
-
-            //Console.WriteLine(Directory.GetCurrentDirectory());
-            //var clientToken = this.FleetServer.RegisterClient(clientReg);
-            //this.Storage.store("token", clientToken);
-            //this.Storage.store("token", "test_token_cool");
-            //Console.WriteLine("Client registered to Server.");
- 
-            //Process.Start(@"..\..\..\FileShare\bin\Debug\FileShare.exe");
-        }
-
-        
-        public void HandleFileReceive(String filename)
-        {
-            //TODO(AL): Calll Router to handle this and pass it to the appropriate client process
         }
 
         private Process RunProcess(String processName)
