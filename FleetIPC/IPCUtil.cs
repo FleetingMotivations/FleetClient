fb6693c6edd2937972e0622bcc8ddbf908d5ae89
Hayden Cheers <hayden.cheers@me.com>
2016-08-28 11:23:19 +1000

    IPC client creating utility

---
+++ FleetIPC/IPCUtil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+
+namespace FleetIPC
+{
+    public static class IPCUtil
+    {
+        public static FleetDaemonClient MakeDaemonClient()
+        {
+            var address = new EndpointAddress("net.pipe://localhost/fleetdaemon");
+            var binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            binding.MaxBufferPoolSize = Int64.MaxValue;
+            binding.MaxBufferSize = Int32.MaxValue;
+            binding.MaxReceivedMessageSize = Int64.MaxValue;
+
+            var client = new FleetDaemonClient(binding, address);
+            return client;
+        }
+
+        public static ApplicationClient MakeApplicationClient(String pipename)
+        {
+            var address = new EndpointAddress("net.pipe://localhost/" + pipename);
+            var binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            binding.MaxBufferPoolSize = Int64.MaxValue;
+            binding.MaxBufferSize = Int32.MaxValue;
+            binding.MaxReceivedMessageSize = Int64.MaxValue;
+
+            var client = new ApplicationClient(binding, address);
+            return client;
+        } 
+        
+        public static ServiceHost MakeApplicationService(String pipename)
+        {
+            var address = new Uri("net.pipe://localhost/fileinbox");
+            var binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+            binding.MaxBufferPoolSize = Int64.MaxValue;
+            binding.MaxBufferSize = Int32.MaxValue;
+            binding.MaxReceivedMessageSize = Int64.MaxValue;
+
+            var service = new ServiceHost(typeof(ApplicationService));
+            service.AddServiceEndpoint(typeof(IApplicationIPC), binding, address);
+            return service;
+        }  
+    }
+}
